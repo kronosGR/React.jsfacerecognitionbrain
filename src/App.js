7eb3299c151d441719c5f8bbc192575d0f50534e
Katsanakis Georgios <geo.elgeo@gmail.com>
2021-10-13 21:52:12 +0200

    Started intergrating Clarifai Api

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'c1f4d7bd2a17465f81c44913eac997b7',
+});
 
 const particlesOptions = {
   number: {
@@ -17,18 +22,50 @@ const particlesOptions = {
   },
 };
 
-function App() {
-  return (
-    <div className='App'>
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target);
+  };
+
+  onButtonSubmit = (event) => {
+    // model_id: '31025e019a18970a1acc55ba6a184dc6',
+    // version_id: 'a5d7776f0c064a41b48c3ce039049f65',
+    app.models
+      .predict(
+        '31025e019a18970a1acc55ba6a184dc6',
+        'https://samples.clarifai.com/face-det.jpg'
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (error) {}
+      );
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/*
       <FaceRecognition /> */}
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import './ImageLinkForm.css'
+import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
@@ -10,8 +10,14 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='pa4 br3 shadow-5 center form'>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>
+          <input
+            className='f4 pa2 w-70 center'
+            type='text'
+            onChange={onInputChange}
+          />
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            onClick={onButtonSubmit}>
             Detect
           </button>
         </div>
